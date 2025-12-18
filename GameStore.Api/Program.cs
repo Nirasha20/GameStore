56a29df1ad4eab0fbe46833e7a21acee5a7f468b
Nirasha <demelwgnd.22@uom.lk>
2025-12-18 14:41:03 +0530

    Refactor GameDto to include properties and initialize sample game data in Program.cs

--- GameStore.Api/Program.cs
+++ GameStore.Api/Program.cs
@@ -1,5 +1,31 @@
+using GameStore.Api.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+
+List<GameDto> games = [
+    new (
+        1,
+        "Street Fighter V",
+        "Fighting",
+        19.99M,
+        new DateOnly(2016, 2, 16)
+    ),
+    new (
+        2,
+        "Final Fantasy VII Remake",
+        "Roleplaying",
+        59.99M,
+        new DateOnly(2020, 4, 10)
+    ),
+    new(
+        3,
+        "FIFA 22",
+        "Sports",
+        69.99M,
+        new DateOnly(2021, 10, 19)
+    )
+];
 
 app.MapGet("/", () => "Hello World!");
 
